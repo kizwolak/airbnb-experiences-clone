f0286b5e4134d7388223bf83913c26cf36f2ee4e
kizwolak <ki.zwolak@gmail.com>
2023-02-16 01:02:24 -0300

    Successfully imported HTML file

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require("path");
+
+module.exports = {
+  "module": {
+    "rules": [
+      {
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "babel-preset-env",
+              "babel-preset-react"
+            ]
+          }
+        },
+        "exclude": /node_modules/,
+        "test": /\.js$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
